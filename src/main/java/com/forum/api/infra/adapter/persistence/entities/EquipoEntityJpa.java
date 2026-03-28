b1eed7fd9666fa7c47e4d5dccffc7e2ffc1f687d
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:08:09 -0300

    feat: Crear clase entidad para la persistencia de clase dominio «Equipo»

---
+++ src/main/java/com/forum/api/infra/adapter/persistence/entities/EquipoEntityJpa.java
@@ -0,0 +1,58 @@
+package com.forum.api.infra.adapter.persistence.entities;
+
+import com.forum.api.domain.Competencias;
+import com.forum.api.domain.Equipo;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "equipos")
+@Getter
+@NoArgsConstructor
+@Setter
+public class EquipoEntityJpa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nombre;
+    private String apodo;
+    private LocalDate fundacion;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "equipo_competencias")
+    @Column(name = "competencias")
+    @Enumerated(EnumType.STRING)
+    private Set<Competencias> competencias;
+
+    public Equipo toDomainExistent() {
+        return Equipo.restore(
+                id,
+                nombre,
+                apodo,
+                fundacion,
+                competencias
+
+        );
+    }
+
+    public Equipo toNewDomain() {
+        return Equipo.create(nombre,
+                apodo,
+                fundacion,
+                competencias);
+    }
+
+    public static EquipoEntityJpa fromDomain(Equipo equipo) {
+        EquipoEntityJpa entity = new EquipoEntityJpa();
+        entity.setId(equipo.getId());
+        entity.setNombre(equipo.getNombre());
+        entity.setApodo(equipo.getApodo());
+        entity.setFundacion(equipo.getFundacion());
+        entity.setCompetencias(equipo.getCompetencias());
+        return entity;
+    }
+}
