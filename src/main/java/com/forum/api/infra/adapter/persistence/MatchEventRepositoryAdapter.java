44d529d26efa02f015b8fd04050b38c7855a831c
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:28:30 -0300

    feat: crear clase implementadora de MatchEventRepository

---
+++ src/main/java/com/forum/api/infra/adapter/persistence/MatchEventRepositoryAdapter.java
@@ -0,0 +1,32 @@
+package com.forum.api.infra.adapter.persistence;
+
+import com.forum.api.application.out.MatchEventRepository;
+import com.forum.api.domain.MatchEvent;
+import com.forum.api.infra.adapter.persistence.entities.MatchEventJpaEntity;
+import com.forum.api.infra.adapter.persistence.repository.MatchEventJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+@Component
+public class MatchEventRepositoryAdapter implements MatchEventRepository {
+    private final MatchEventJpaRepository repository;
+
+    public MatchEventRepositoryAdapter(MatchEventJpaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public MatchEvent saveMatchEvent(MatchEvent matchEvent) {
+        return repository.save(MatchEventJpaEntity.fromDomain(matchEvent)).toDomain();
+    }
+
+    @Override
+    public void deleteMatchEvent(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<MatchEvent> findMatchEventById(Long id) {
+        return Optional.empty();
+    }
+}
