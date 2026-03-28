aef0c83d7835a8d9b7b499973a440a1b343a6b63
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:26:56 -0300

    feat: crear clase implementadora de MatchEventService

---
+++ src/main/java/com/forum/api/application/service/MatchEventServiceImpl.java
@@ -0,0 +1,29 @@
+package com.forum.api.application.service;
+
+import com.forum.api.application.in.MatchEventService;
+import com.forum.api.domain.MatchEvent;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class MatchEventServiceImpl implements MatchEventService {
+    @Override
+    public MatchEvent agregarNuevoMatchEvent(MatchEvent matchEvent) {
+        return null;
+    }
+
+    @Override
+    public void borrarMatchEvent(Long id) {
+
+    }
+
+    @Override
+    public Set<MatchEvent> listarMatchEvents() {
+        return Set.of();
+    }
+
+    @Override
+    public MatchEvent encotrarMatchEvent(Long id) {
+        return null;
+    }
+}
