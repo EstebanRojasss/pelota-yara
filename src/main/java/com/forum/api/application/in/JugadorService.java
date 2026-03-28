f40a5d014b6a980a86594e22f75a6c3f7d25a0a9
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:30:33 -0300

    feat: crear interface service y clase implementadora de JugadorService

---
+++ src/main/java/com/forum/api/application/in/JugadorService.java
@@ -0,0 +1,14 @@
+package com.forum.api.application.in;
+
+import com.forum.api.domain.Jugador;
+
+
+import java.util.List;
+
+public interface JugadorService {
+    Jugador agregarNuevoJugador(Jugador jugador);
+    List<Jugador> listarJugadoresEquipo(Long equipoId);
+    Jugador encontrarJugadorPorId(Long id);
+    void eliminarJugadorPorId(Long id);
+    Jugador actualizarDatosJugador(Jugador jugador);
+}
