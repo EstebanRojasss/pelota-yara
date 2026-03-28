6ebf4e963d442742963bac518b329e1fdc4f1e5b
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:02:30 -0300

    feat: crear clase Jugador con sus respectivos metodos de creacion y restauracion

--- src/main/java/com/forum/api/domain/Jugador.java
+++ src/main/java/com/forum/api/domain/Jugador.java
@@ -7,4 +7,41 @@ public class Jugador {
     private String nacionalidad;
 
     private Equipo equipo;
+
+    private Jugador(Long id, String nombre, Integer edad, String nacionalidad, Equipo equipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.equipo = equipo;
+    }
+
+
+    public static Jugador create(String nombre, Integer edad, String nacionalidad, Equipo equipo){
+        return new Jugador(null, nombre, edad, nacionalidad, equipo);
+    }
+
+    public static Jugador restore(Long id, String nombre, Integer edad, String nacionalidad, Equipo equipo){
+        return new Jugador(id, nombre, edad, nacionalidad, equipo);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
 }
