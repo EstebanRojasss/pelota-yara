fe5021e04c126a59da1df1d4607b7d1cce673abb
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:28:57 -0300

    feat: crear clase Estadio en el dominio

--- src/main/java/com/forum/api/domain/Estadio.java
+++ src/main/java/com/forum/api/domain/Estadio.java
@@ -9,4 +9,22 @@ public class Estadio {
     private LocalDate fundacion;
     private String direccion;
     private String ciudad;
+
+    private Equipo equipo;
+
+    public Estadio(Long id,
+                   String nombre,
+                   Integer capacidad,
+                   LocalDate fundacion,
+                   String direccion,
+                   String ciudad,
+                   Equipo equipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.fundacion = fundacion;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.equipo = equipo;
+    }
 }
