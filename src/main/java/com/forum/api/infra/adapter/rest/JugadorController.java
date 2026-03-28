f6e5469f17ab27a12bd07def0a7df6c4b7f0e69c
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:31:13 -0300

    feat: crear controller para manejo de datos de «Jugador»

---
+++ src/main/java/com/forum/api/infra/adapter/rest/JugadorController.java
@@ -0,0 +1,39 @@
+package com.forum.api.infra.adapter.rest;
+
+import com.forum.api.application.in.JugadorService;
+import com.forum.api.infra.adapter.rest.dto.JugadorRequestDto;
+import com.forum.api.infra.adapter.rest.dto.JugadorResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+public class JugadorController {
+
+    private final JugadorService jugadorService;
+
+    public JugadorController(JugadorService jugadorService) {
+        this.jugadorService = jugadorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<JugadorResponseDto> agregarNuevoJugador(@RequestBody JugadorRequestDto jugadorDto) {
+        JugadorResponseDto response = JugadorResponseDto
+                .fromDomain(
+                        jugadorService.agregarNuevoJugador(jugadorDto.toDomain())
+                );
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+
+}
