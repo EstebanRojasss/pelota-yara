e28ae23664ec2b7c48db159dace2836e3f13da11
ESTEBAN <estebanrojasch.2@gmail.com>
2026-03-28 19:24:11 -0300

    feat: crear clase adapter de jpaRepositoryPartido

---
+++ src/main/java/com/forum/api/infra/adapter/persistence/PartidoJpaRepositoryAdpater.java
@@ -0,0 +1,35 @@
+package com.forum.api.infra.adapter.persistence;
+
+import com.forum.api.application.out.PartidoRepository;
+import com.forum.api.domain.Partido;
+import com.forum.api.infra.adapter.persistence.entities.PartidoJpaEntity;
+import com.forum.api.infra.adapter.persistence.repository.PartidoJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class PartidoJpaRepositoryAdpater implements PartidoRepository {
+
+    private final PartidoJpaRepository repository;
+
+    public PartidoJpaRepositoryAdpater(PartidoJpaRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public Partido savePartido(Partido partido) {
+        return repository.save(PartidoJpaEntity.fromDomain(partido)).toDomainExistent();
+    }
+
+    @Override
+    public Optional<Partido> findPartidoById(Long id) {
+        return repository.findById(id).map(PartidoJpaEntity::toDomainExistent);
+    }
+
+    @Override
+    public void deletePartido(Long id) {
+        repository.deleteById(id);
+    }
+}
