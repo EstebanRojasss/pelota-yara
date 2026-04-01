3e9982667005d4ddb7cb63b547b13ad75deffcfa
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-01 18:10:39 -0300

    feat: agregar clase enum de competencias

---
+++ src/main/java/com/forum/api/domain/Competencias.java
@@ -0,0 +1,48 @@
+package com.forum.api.domain;
+
+import com.forum.api.domain.exception.CompetenciaNotFoundException;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public enum Competencias {
+    APERTURA("apertura"),
+    CLAUSURA("clausura"),
+    INTEGRADO("integrado"),
+    COPA_PY("copa_py");
+
+
+    private final String valor;
+
+    private final static Map<String, Competencias> NOMBRES = Arrays
+            .stream(Competencias.values())
+            .collect(Collectors.toMap(Competencias::getValor, c -> c));
+
+    Competencias(String valor) {
+        this.valor = valor;
+    }
+
+    private static Optional<Competencias> from(String nombre){
+        return Optional.ofNullable(NOMBRES.get(nombre));
+    }
+
+    public static Competencias fromValor(String nombreCompetencia){
+
+        String normalizacion = nombreCompetencia
+                .toLowerCase()
+                .replace("-", "_");
+
+        return from(normalizacion).orElseThrow(
+                () -> new CompetenciaNotFoundException("Competencia inválida ->> " + nombreCompetencia));
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public boolean comprobarCompetencia(String nombre){
+        return NOMBRES.containsKey(nombre);
+    }
+}
