68958ebee1a3fdedf8cf3de59b3c2e7c1ee88bf8
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-03 18:52:26 -0300

    refactor: cambiar nombre de atributo de enum

--- src/main/java/com/forum/api/domain/model/StatusPartido.java
+++ src/main/java/com/forum/api/domain/model/StatusPartido.java
@@ -2,7 +2,7 @@ package com.forum.api.domain.model;
 
 public enum StatusPartido {
     FINALIZADO,
-    INICIO_SEGUNDO_TIEMPO,
+    SEGUNDO_TIEMPO,
     MEDIO_TIEMPO,
     NO_INICIADO,
     EN_JUEGO
