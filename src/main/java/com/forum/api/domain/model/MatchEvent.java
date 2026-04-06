2281b25d5b4c19736735005e74a14ccc1229f8da
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-06 10:34:05 -0300

    feat: agrear metodo para agrear partido al set

--- src/main/java/com/forum/api/domain/model/MatchEvent.java
+++ src/main/java/com/forum/api/domain/model/MatchEvent.java
@@ -38,6 +38,10 @@ public class MatchEvent {
         };
     }
 
+    private void addPartido(Partido partido){
+        this.partidos.add(partido);
+    }
+
 
     public Long getId() {
         return id;
