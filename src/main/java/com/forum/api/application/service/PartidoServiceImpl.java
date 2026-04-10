56a3e547f47ced43b8280d11b6f21e7892e87a07
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-09 21:24:47 -0300

    fix: eliminar palabras «this» innecesarias

--- src/main/java/com/forum/api/application/service/PartidoServiceImpl.java
+++ src/main/java/com/forum/api/application/service/PartidoServiceImpl.java
@@ -24,37 +24,36 @@ implements PartidoService {
     }
 
     public Partido encontrarPartido(Long id) {
-        return (Partido)this.partidoRepository.findPartidoById(id).orElseThrow(() -> new PartidoNotFoundException("El partido no se encuentra"));
+        return partidoRepository.findPartidoById(id).orElseThrow(() -> new PartidoNotFoundException("El partido no se encuentra"));
     }
 
     public void borrarPartido(Long id) {
         try {
-            this.partidoRepository.deletePartido(id);
-        }
-        catch (RuntimeException e) {
+            partidoRepository.deletePartido(id);
+        } catch (RuntimeException e) {
             throw new PartidoNotFoundException("El partido no se encuentra");
         }
     }
 
     public Partido guardarNuevoPartido(CrearPartidoCommand partidoCommand) {
-        Equipo equipoLocal = this.equipoService.encontrarEquipoPorId(partidoCommand.equipoLocalId());
-        Equipo equipoVisitante = this.equipoService.encontrarEquipoPorId(partidoCommand.equipoVisitanteId());
-        Partido partido = Partido.create((Equipo)equipoLocal, (Equipo)equipoVisitante);
-        return this.partidoRepository.savePartido(partido);
+        Equipo equipoLocal = equipoService.encontrarEquipoPorId(partidoCommand.equipoLocalId());
+        Equipo equipoVisitante = equipoService.encontrarEquipoPorId(partidoCommand.equipoVisitanteId());
+        Partido partido = Partido.create(equipoLocal, equipoVisitante);
+        return partidoRepository.savePartido(partido);
     }
 
     public Partido actualizarDatosDePartido(Partido datosPartidoActualizar) {
         this.partidoRepository.findPartidoById(datosPartidoActualizar.getId()).orElseThrow(() -> new PartidoNotFoundException("Partido no encontrado."));
-        return this.partidoRepository.savePartido(datosPartidoActualizar);
+        return partidoRepository.savePartido(datosPartidoActualizar);
     }
 
     public List<Partido> encontrarTodosLosPartidosEnVivo() {
         StatusPartido enVivo = StatusPartido.EN_JUEGO;
-        return this.partidoRepository.findPartidosByStatus(enVivo);
+        return partidoRepository.findPartidosByStatus(enVivo);
     }
 
     public List<Partido> listarTodosLosPartidos() {
-        return this.partidoRepository.findAllPartidos();
+        return partidoRepository.findAllPartidos();
     }
 }
 
