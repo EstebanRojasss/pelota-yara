e1a4049534a2e5cb33aeabb0e9fd5b4ad1ad71df
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-11 23:50:43 -0300

    refactor: cambiar parametro de creación de «Equipo» -» «EquipoPartidoDto»

--- src/main/java/com/forum/api/infra/adapter/in/rest/dto/PartidoResponseDto.java
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/PartidoResponseDto.java
@@ -1,16 +1,16 @@
 package com.forum.api.infra.adapter.in.rest.dto;
 
-import com.forum.api.domain.model.Equipo;
+
 import com.forum.api.domain.model.Partido;
 import com.forum.api.domain.model.StatusPartido;
 
-public record PartidoResponseDto(Long id, StatusPartido status, Equipo equipoLocal, Equipo equipoVisitante, Integer golVisitante, Integer golLocal, Integer minutoActual, Integer minutoAdicional1T, Integer minutoAdicional2T) {
+public record PartidoResponseDto(Long id, StatusPartido status,EquipoPartidoDto equipoLocal, EquipoPartidoDto equipoVisitante, Integer golVisitante, Integer golLocal, Integer minutoActual, Integer minutoAdicional1T, Integer minutoAdicional2T) {
 
     public static PartidoResponseDto fromDomainExistent(Partido partido) {
         return new PartidoResponseDto(partido.getId(),
                 partido.getStatus(),
-                partido.getEquipoLocal(),
-                partido.getEquipoVisitante(),
+                EquipoPartidoDto.fromDomain(partido.getEquipoLocal()),
+                EquipoPartidoDto.fromDomain(partido.getEquipoVisitante()),
                 partido.getGolVisitante(),
                 partido.getGolLocal(),
                 partido.getMinutoActual(),
