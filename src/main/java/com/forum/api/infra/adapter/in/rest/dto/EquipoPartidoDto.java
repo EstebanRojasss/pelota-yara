bb9d4dc944308ed68459d0fa753cd77b86c9633d
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-11 23:57:08 -0300

    feat: agregar «EquipoDto» para parametro de «PartidoDto»

---
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoPartidoDto.java
@@ -0,0 +1,13 @@
+package com.forum.api.infra.adapter.in.rest.dto;
+
+import com.forum.api.domain.model.Equipo;
+
+public record EquipoPartidoDto(Long id, String nombre) {
+
+    public static EquipoPartidoDto fromDomain(Equipo equipo){
+        return new EquipoPartidoDto(
+                equipo.getId(),
+                equipo.getNombre()
+        );
+    }
+}
