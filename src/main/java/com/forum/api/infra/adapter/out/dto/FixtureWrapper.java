a55df5aa8f1b526d83f4906b518c93919458ece0
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-18 14:29:18 -0300

    feat: agregar propiedad json

--- src/main/java/com/forum/api/infra/adapter/out/dto/FixtureWrapper.java
+++ src/main/java/com/forum/api/infra/adapter/out/dto/FixtureWrapper.java
@@ -1,10 +1,17 @@
 package com.forum.api.infra.adapter.out.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FixtureWrapper {
+    @JsonProperty("fixture")
     private Fixture fixture;
+    @JsonProperty("teams")
     private Teams teams;
+    @JsonProperty("goals")
     private Goals goals;
+    @JsonProperty("events")
     private List<Event> events;
 }
