7766b5e3b74af20a8f9762bcd594363e3b951f74
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-18 13:30:11 -0300

    feat: crear wrapper de fixtures para recibir formato json

---
+++ src/main/java/com/forum/api/infra/adapter/out/dto/FixtureWrapper.java
@@ -0,0 +1,10 @@
+package com.forum.api.infra.adapter.out.dto;
+
+import java.util.List;
+
+public class FixtureWrapper {
+    private Fixture fixture;
+    private Teams teams;
+    private Goals goals;
+    private List<Event> events;
+}
