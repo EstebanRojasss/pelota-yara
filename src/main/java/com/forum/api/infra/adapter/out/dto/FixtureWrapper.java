3fa66ea1f2ce1a6bebb8d1bdcc164003d47bfc1c
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-19 20:55:13 -0300

    feat: agregar mapper a FixtureWrapper

--- src/main/java/com/forum/api/infra/adapter/out/dto/FixtureWrapper.java
+++ src/main/java/com/forum/api/infra/adapter/out/dto/FixtureWrapper.java
@@ -2,6 +2,9 @@ package com.forum.api.infra.adapter.out.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.forum.api.application.in.dto.FixtureData;
+import com.forum.api.application.in.dto.StatusPartidoFixture;
+import com.forum.api.application.in.dto.TeamDataDto;
 import com.forum.api.infra.adapter.out.dto.fixture.Fixture;
 import com.forum.api.infra.adapter.out.dto.goals.Goals;
 import com.forum.api.infra.adapter.out.dto.team.Teams;
@@ -16,6 +19,20 @@ public record FixtureWrapper(
         @JsonProperty("goals")
         Goals goals
 ) {
-
+        public static FixtureData map(FixtureWrapper wrapper){
+                return new FixtureData(
+                        wrapper.fixture.id(),
+                        TeamDataDto.map(
+                                wrapper.teams.home().id(),
+                                wrapper.teams.home().name()),
+                        TeamDataDto.map(
+                                wrapper.teams.away().id(),
+                                wrapper.teams.away().name()),
+                        wrapper.goals.home(),
+                        wrapper.goals.away(),
+                        wrapper.fixture.status().elapsed(),
+                        StatusPartidoFixture.fromShortValue(wrapper.fixture.status().shortStatus())
+                );
+        }
 
 }
