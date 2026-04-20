13314f181d3c872217fded49e06ed4a10ad5b05e
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-20 00:01:32 -0300

    style: cambiar nombre

--- src/main/java/com/forum/api/application/out/EquipoRepository.java
+++ src/main/java/com/forum/api/application/out/EquipoRepository.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EquipoRepository {
-     Equipo save(Equipo var1);
+     Equipo save(Equipo equipo);
 
-     void delete(Long var1);
+     void delete(Long id);
 
-     Optional<Equipo> findEquipoById(Long var1);
+     Optional<Equipo> findEquipoById(Long id);
 
      List<Equipo> findAllEquipos();
 }
