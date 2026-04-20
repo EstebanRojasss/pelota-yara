1dade22a8a831660c39fe812f25bfad8ef01d959
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-20 00:00:01 -0300

    style

--- src/main/java/com/forum/api/infra/adapter/out/persistence/repository/EquipoJpaRepository.java
+++ src/main/java/com/forum/api/infra/adapter/out/persistence/repository/EquipoJpaRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EquipoJpaRepository extends JpaRepository<EquipoEntityJpa, Long> {
-    Optional<EquipoEntityJpa> findByNombre(String var1);
+    Optional<EquipoEntityJpa> findByNombre(String nombre);
 
 
 }
