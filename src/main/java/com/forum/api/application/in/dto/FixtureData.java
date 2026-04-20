e904135a7cedd1d9065d7b5e43415049603bcc64
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-20 00:10:20 -0300

    feat: agregar dto de fixture en capa Application

---
+++ src/main/java/com/forum/api/application/in/dto/FixtureData.java
@@ -0,0 +1,14 @@
+package com.forum.api.application.in.dto;
+
+
+public record FixtureData(
+        Long id,
+        TeamDataDto local,
+        TeamDataDto visitante,
+        Integer golLocal,
+        Integer golVisitante,
+        Integer minuto,
+        StatusPartidoFixture statusFixture
+) {
+
+}
