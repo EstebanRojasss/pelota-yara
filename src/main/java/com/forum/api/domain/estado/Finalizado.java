061cafa0c0be96222de067697ebc6a48fb09b9a4
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-22 23:42:24 -0300

    refactor: cambiar nombre de estado

--- src/main/java/com/forum/api/domain/estado/Finalizado.java
+++ src/main/java/com/forum/api/domain/estado/Finalizado.java
@@ -5,8 +5,8 @@ import com.forum.api.domain.model.StatusPartido;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FinPartido implements EstadoPartido {
-    private static final Logger log = LoggerFactory.getLogger(FinPartido.class);
+public class Finalizado implements EstadoPartido {
+    private static final Logger log = LoggerFactory.getLogger(Finalizado.class);
 
     public void ejecutar(Partido partido) {
         partido.setStatus(StatusPartido.FINALIZADO);
