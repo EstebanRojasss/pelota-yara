3d06b397b66d5f5b96af3db5549d8556e19f88a8
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-25 20:59:47 -0300

    feat: agregar nuevo parametro logo a response dto

--- src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoPartidoDto.java
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoPartidoDto.java
@@ -2,12 +2,13 @@ package com.forum.api.infra.adapter.in.rest.dto;
 
 import com.forum.api.domain.model.Equipo;
 
-public record EquipoPartidoDto(Long id, String nombre) {
+public record EquipoPartidoDto(Long id, String nombre, String logo) {
 
     public static EquipoPartidoDto fromDomain(Equipo equipo){
         return new EquipoPartidoDto(
                 equipo.getId(),
-                equipo.getNombre()
+                equipo.getNombre(),
+                equipo.getLogo()
         );
     }
 }
