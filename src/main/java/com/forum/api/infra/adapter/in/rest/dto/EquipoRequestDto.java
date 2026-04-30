e6b418f367959ee27587a45dd5dc5754e897124d
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-30 01:02:56 -0300

    refactor: eliminar parametros: competencias

--- src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoRequestDto.java
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoRequestDto.java
@@ -1,21 +1,16 @@
 package com.forum.api.infra.adapter.in.rest.dto;
 
-import com.forum.api.domain.model.Competencias;
 import com.forum.api.domain.model.Equipo;
 
 import java.time.LocalDate;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-public record EquipoRequestDto(Long id, String nombre, String apodo, LocalDate fundacion, Set<String> competencias) {
+public record EquipoRequestDto(Long id, String nombre, String apodo, LocalDate fundacion, Long equipoFixtureId, String logo) {
     public Equipo toDomain() {
         return Equipo.create(nombre,
                 apodo,
                 fundacion,
-                competencias
-                        .stream()
-                        .map(Competencias::fromValor)
-                        .collect(Collectors.toSet()));
+                equipoFixtureId,
+                logo);
     }
 
     public Equipo toDomainExistent() {
@@ -23,10 +18,8 @@ public record EquipoRequestDto(Long id, String nombre, String apodo, LocalDate f
                 nombre,
                 apodo,
                 fundacion,
-                competencias
-                        .stream()
-                        .map(Competencias::fromValor)
-                        .collect(Collectors.toSet()));
+                equipoFixtureId,
+                logo);
     }
 
 }
--- src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoResponseDto.java
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/EquipoResponseDto.java
@@ -1,23 +1,17 @@
 package com.forum.api.infra.adapter.in.rest.dto;
 
-import com.forum.api.domain.model.Competencias;
 import com.forum.api.domain.model.Equipo;
 
 import java.time.LocalDate;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-public record EquipoResponseDto(Long id, String nombre, String apodo, LocalDate fundacion, Set<String> competencias) {
+
+public record EquipoResponseDto(Long id, String nombre, String apodo, LocalDate fundacion) {
     public static EquipoResponseDto fromDomain(Equipo equipo) {
         return new EquipoResponseDto(
                 equipo.getId(),
                 equipo.getNombre(),
                 equipo.getApodo(),
-                equipo.getFundacion(),
-                equipo.getCompetencias().
-                        stream().
-                        map(Competencias::getValor).
-                        collect(Collectors.toSet()));
+                equipo.getFundacion());
     }
 }
 
