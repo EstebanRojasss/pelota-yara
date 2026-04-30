09ed6d0adba683dee387c1096067cb9ab8e8e5f2
ESTEBAN <estebanrojasch.2@gmail.com>
2026-04-30 00:46:30 -0300

    feat: agregar metodo para calcular minuto en estados

--- src/main/java/com/forum/api/domain/estado/EstadoPartido.java
+++ src/main/java/com/forum/api/domain/estado/EstadoPartido.java
@@ -3,7 +3,8 @@ package com.forum.api.domain.estado;
 import com.forum.api.domain.model.Partido;
 
 public interface EstadoPartido {
-     void ejecutar(Partido var1);
+     void ejecutar(Partido partido);
 
+     Integer calcularMinutoActual(Partido partido);
 }
 
