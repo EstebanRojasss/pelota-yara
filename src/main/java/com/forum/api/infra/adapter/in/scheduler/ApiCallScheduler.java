0044c21daf96cba6027ef7cf92c437df9f17d496
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-02 21:48:26 -0300

    refactor: cambiar metodo de scheduled agregando response de lista de partidos

--- src/main/java/com/forum/api/infra/adapter/in/scheduler/ApiCallScheduler.java
+++ src/main/java/com/forum/api/infra/adapter/in/scheduler/ApiCallScheduler.java
@@ -1,22 +1,38 @@
 package com.forum.api.infra.adapter.in.scheduler;
 
 import com.forum.api.application.in.PartidoService;
+import com.forum.api.application.in.SSeBroadcastUseCase;
 import com.forum.api.domain.model.Partido;
+import com.forum.api.infra.adapter.in.rest.dto.PartidoResponseDto;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ApiCallScheduler {
 
     private final PartidoService partidoService;
+    private final SSeBroadcastUseCase broadcastUseCase;
 
-    public ApiCallScheduler(PartidoService partidoService) {
+    public ApiCallScheduler(PartidoService partidoService, SSeBroadcastUseCase broadcastUseCase) {
         this.partidoService = partidoService;
+        this.broadcastUseCase = broadcastUseCase;
     }
 
     @Scheduled(fixedRate = 60000)
     public void llamarApiFootballVivo(){
-        partidoService.encontrarTodosLosPartidosEnVivo().
-                forEach(Partido::ejecutar);
+       List<Partido> partidos = partidoService.encontrarTodosLosPartidosEnVivo();
+       partidos.forEach(Partido::ejecutar);
+
+       List<PartidoResponseDto> partidosDto = partidos
+               .stream()
+               .map( p ->{
+                   p.actualizarMinutoActual();
+                   return PartidoResponseDto.fromDomainExistent(p);
+               })
+               .toList();
+
+       broadcastUseCase.broadcast(partidosDto);
     }
 }
