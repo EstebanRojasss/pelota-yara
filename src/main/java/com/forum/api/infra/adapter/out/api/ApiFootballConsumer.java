798a0645e60f9d120c17619180d4bbae97c2d10f
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-02 21:50:13 -0300

    feat: agregar nuevo proveedor de datos de equipos de api externa, además de agregar filtrado de ligas especificas en el response de la api externa

--- src/main/java/com/forum/api/infra/adapter/out/api/ApiFootballConsumer.java
+++ src/main/java/com/forum/api/infra/adapter/out/api/ApiFootballConsumer.java
@@ -1,8 +1,12 @@
 package com.forum.api.infra.adapter.out.api;
 
-import com.forum.api.application.in.DataFixtureProvider;
+import com.forum.api.application.in.DataApiProvider;
 import com.forum.api.application.in.dto.FixtureData;
+import com.forum.api.application.in.dto.TeamDataDto;
 import com.forum.api.infra.adapter.out.dto.FixtureWrapper;
+import com.forum.api.infra.adapter.out.dto.TeamWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -12,11 +16,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.function.Predicate;
 
 @Component
-public class ApiFootballConsumer implements DataFixtureProvider {
+public class ApiFootballConsumer implements DataApiProvider {
 
     private final RestTemplate restTemplate;
 
@@ -29,15 +36,12 @@ public class ApiFootballConsumer implements DataFixtureProvider {
 
     @Override
     public List<FixtureData> proveerDatosFixture() {
-
-
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-apisports-key", apiKey);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
-
         ResponseEntity<ApiResponse<FixtureWrapper>> response = restTemplate.exchange(
-                "https://v3.football.api-sports.io/fixtures?live=all&league=13",
+                "https://v3.football.api-sports.io/fixtures?live=all",
                 HttpMethod.GET,
                 entity,
                 new ParameterizedTypeReference<ApiResponse<FixtureWrapper>>() {
@@ -52,6 +56,41 @@ public class ApiFootballConsumer implements DataFixtureProvider {
         return body.getResponse().
                 stream().
                 map(FixtureWrapper::map).
-                collect(Collectors.toList());
+                filter(
+                        filtrarLiga(List.of(250L, 130L, 128L, 1032L, 2L, 3L, 73L, 13L, 11L, 836L, 194L))
+                ).toList();
+    }
+
+    @Override
+    public List<TeamDataDto> proveerDatosEquipos() {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("x-apisports-key", apiKey);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<ApiResponse<TeamWrapper>> response = restTemplate.exchange(
+                "https://v3.football.api-sports.io/teams?league=252&season=2024",
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<ApiResponse<TeamWrapper>>() {
+                }
+        );
+
+        if (response.getBody() == null) {
+            throw new IllegalStateException("Respuesta vacía de API FOOTBALL");
+        }
+
+        return response.
+                getBody().
+                getResponse().
+                stream().
+                map(TeamWrapper::map)
+                .toList();
+    }
+
+    private Predicate<FixtureData> filtrarLiga(Collection<Long> idsPermitidos) {
+        Set<Long> permitidos = new HashSet<>(idsPermitidos);
+        return f -> f != null && permitidos.contains(f.ligaId());
     }
 }
