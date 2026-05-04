afc6756fe2729d99db130baca3e1d15934500d1c
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-03 21:24:27 -0300

    feat: agregar maper en dtoLiga

--- src/main/java/com/forum/api/application/in/dto/LigaDataDto.java
+++ src/main/java/com/forum/api/application/in/dto/LigaDataDto.java
@@ -6,4 +6,12 @@ public record LigaDataDto(
         String pais,
         Integer temporada
 ) {
+
+    public static LigaDataDto map(Long id, String nombre, String pais, Integer temporada){
+        return new LigaDataDto(
+                id,
+                nombre,
+                pais,
+                temporada);
+    }
 }
