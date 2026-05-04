49ca419a2d536f6f54ec0c2a7752593dccee298b
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-03 22:05:12 -0300

    feat: agregar adapter de repositorio de Liga

---
+++ src/main/java/com/forum/api/infra/adapter/out/persistence/LigaJpaRepositoryAdapter.java
@@ -0,0 +1,20 @@
+package com.forum.api.infra.adapter.out.persistence;
+
+import com.forum.api.application.out.LigaRepository;
+import com.forum.api.domain.model.Liga;
+import com.forum.api.infra.adapter.out.persistence.entities.LigaJpaEntity;
+import com.forum.api.infra.adapter.out.persistence.repository.LigaJpaRepository;
+
+public class LigaJpaRepositoryAdapter implements LigaRepository {
+
+    private final LigaJpaRepository ligaJpaRepository;
+
+    public LigaJpaRepositoryAdapter(LigaJpaRepository ligaJpaRepository) {
+        this.ligaJpaRepository = ligaJpaRepository;
+    }
+
+    @Override
+    public Liga save(Liga liga) {
+        return ligaJpaRepository.save(LigaJpaEntity.fromDomain(liga)).toDomainExistent();
+    }
+}
