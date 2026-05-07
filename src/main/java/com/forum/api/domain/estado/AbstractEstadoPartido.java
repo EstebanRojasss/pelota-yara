a96d212b117b71aa6e35425fe34dac118985108e
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-06 23:45:56 -0300

    feat: crear clase abstracta de EstadoPartido

---
+++ src/main/java/com/forum/api/domain/estado/AbstractEstadoPartido.java
@@ -0,0 +1,22 @@
+package com.forum.api.domain.estado;
+
+
+import com.forum.api.domain.model.StatusPartido;
+
+public  abstract class AbstractEstadoPartido implements EstadoPartido {
+
+
+    public boolean partidoEnJuego(StatusPartido status) {
+        return status.equals(StatusPartido.PRIMER_TIEMPO) ||
+                status.equals(StatusPartido.SEGUNDO_TIEMPO) ||
+                status.equals(StatusPartido.MEDIO_TIEMPO) ||
+                status.equals(StatusPartido.TIEMPO_EXTRA);
+    }
+
+    @Override
+    public String nombreEstado() {
+        return "";
+    }
+
+
+}
