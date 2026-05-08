4f4c6f6d2638707bf2a5b79875b5afe673892462
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-08 19:51:59 -0300

    feat: implementar nuevos metodos en interface EstadoPartido

--- src/main/java/com/forum/api/domain/estado/EstadoPartido.java
+++ src/main/java/com/forum/api/domain/estado/EstadoPartido.java
@@ -6,5 +6,15 @@ public interface EstadoPartido {
      void ejecutar(Partido partido);
 
      Integer calcularMinutoActual(Partido partido);
+
+     EstadoPartido siguienteEstado(Partido partido);
+
+     String nombreEstado();
+
+     default void onEnter(Partido partido){}
+
+     default void onExit(Partido partido){}
+
+
 }
 
