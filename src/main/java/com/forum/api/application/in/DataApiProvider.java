0088fab9f74439676bf464e2124e3df4cbbdda6f
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-08 23:48:38 -0300

    feat: agregar nuevo provider de datos de eventos de partido

--- src/main/java/com/forum/api/application/in/DataApiProvider.java
+++ src/main/java/com/forum/api/application/in/DataApiProvider.java
@@ -3,10 +3,12 @@ package com.forum.api.application.in;
 
 import com.forum.api.application.in.dto.FixtureData;
 import com.forum.api.application.in.dto.TeamDataDto;
+import com.forum.api.infra.adapter.out.dto.event.EventDataDto;
 
 import java.util.List;
 
 public interface DataApiProvider {
     List<FixtureData> proveerDatosFixture();
     List<TeamDataDto> proveerDatosEquipos();
+    List<EventDataDto> proveerEventosPartido();
 }
