bc140ef0a1a862456eefb3e9ba5905ccd57bf040
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-09 18:34:05 -0300

    feat: agregar nuevo parametro de entrada fixtureId

--- src/main/java/com/forum/api/infra/adapter/in/rest/dto/JugadorRequestDto.java
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/JugadorRequestDto.java
@@ -3,11 +3,11 @@ package com.forum.api.infra.adapter.in.rest.dto;
 import com.forum.api.domain.model.Jugador;
 
 
-public record JugadorRequestDto(String nombre, Integer edad, String nacionalidad, EquipoRequestDto equipo) {
+public record JugadorRequestDto(String nombre, Integer edad, String nacionalidad, EquipoRequestDto equipo, Long fixtureId) {
     public Jugador toDomain() {
         return Jugador.create(nombre,
                 edad,
-                nacionalidad,
+                fixtureId,
                 equipo.toDomain());
     }
 }
