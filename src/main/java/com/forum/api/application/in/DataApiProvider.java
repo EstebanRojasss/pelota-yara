789dbdf02dc478a7d57864f157d1d264b0a57c67
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-09 00:06:18 -0300

    refactor: cambiar dato recibido

--- src/main/java/com/forum/api/application/in/DataApiProvider.java
+++ src/main/java/com/forum/api/application/in/DataApiProvider.java
@@ -1,9 +1,9 @@
 package com.forum.api.application.in;
 
 
+import com.forum.api.application.in.dto.EventoDataDto;
 import com.forum.api.application.in.dto.FixtureData;
 import com.forum.api.application.in.dto.TeamDataDto;
-import com.forum.api.infra.adapter.out.dto.event.EventDataDto;
 
 import java.util.List;
 
