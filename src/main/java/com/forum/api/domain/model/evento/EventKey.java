1970071ce17a32189a7276b9c53635f4998762ab
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-14 18:47:27 -0300

    feat: agregar eventKey para mayor consistencia e idempotencia

--- src/main/java/com/forum/api/domain/model/evento/EventKey.java
+++ src/main/java/com/forum/api/domain/model/evento/EventKey.java
@@ -1,5 +1,17 @@
 package com.forum.api.domain.model.evento;
 
-public class EventKey {
+import com.forum.api.application.in.dto.evento.EventoDataDto;
+
+public record EventKey(
+        Integer minuto,
+        Long jugadorApiId,
+        String tipoEvento
+) {
+
+    public static EventKey from(EventoDataDto eventData){
+        return new EventKey(eventData.time(),
+                eventData.playerEvent().id(),
+                eventData.eventType().type());
+    }
 
 }
