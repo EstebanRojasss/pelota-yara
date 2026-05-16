8e27d659b56d4fac8f1ad6438f838a5276b66084
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-16 19:28:05 -0300

    refactor: eliminar parametro «nacionalidad» en el response

--- src/main/java/com/forum/api/infra/adapter/in/rest/dto/JugadorResponseDto.java
+++ src/main/java/com/forum/api/infra/adapter/in/rest/dto/JugadorResponseDto.java
@@ -2,12 +2,11 @@ package com.forum.api.infra.adapter.in.rest.dto;
 
 import com.forum.api.domain.model.Jugador;
 
-public record JugadorResponseDto(Long id, String nombre, Integer edad, String nacionalidad, EquipoResponseDto responseDto) {
+public record JugadorResponseDto(Long id, String nombre, Integer edad, EquipoResponseDto responseDto) {
     public static JugadorResponseDto fromDomain(Jugador jugador) {
         return new JugadorResponseDto(jugador.getId(),
                 jugador.getNombre(),
                 jugador.getEdad(),
-                jugador.getNacionalidad(),
                 EquipoResponseDto.fromDomain(jugador.getEquipo()));
     }
 
