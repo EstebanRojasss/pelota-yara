adf66a32133c3207f54b5a815f1e6d5570e56878
ESTEBAN <estebanrojasch.2@gmail.com>
2026-05-16 19:58:01 -0300

    refactor: nombre

--- src/main/java/com/forum/api/application/in/EventoDelPartidoService.java
+++ src/main/java/com/forum/api/application/in/EventoDelPartidoService.java
@@ -1,15 +1,13 @@
 package com.forum.api.application.in;
 
-import com.forum.api.domain.model.Jugador;
 import com.forum.api.domain.model.evento.EventoDelPartido;
 import com.forum.api.domain.model.partido.Partido;
 
 import java.util.List;
-import java.util.Set;
 
 public interface EventoDelPartidoService {
 
-     EventoDelPartido agregarNuevoEventoDelPartido(EventoDelPartido eventoDelPartido);
+     EventoDelPartido agregarEvento(EventoDelPartido eventoDelPartido);
 
      void borrarEventoDelPartido(Long id);
 
@@ -18,5 +16,10 @@ public interface EventoDelPartidoService {
      EventoDelPartido encontrarEventoDelPartido(Long id);
 
      List<EventoDelPartido> listarEventosDelPartidoAPI(Partido partido);
+
+     List<EventoDelPartido> obtenerEventosDelProvider(Partido partido);
+
+     void agregarEventosPorFase(List<EventoDelPartido> eventoDelPartido);
+
 }
 
